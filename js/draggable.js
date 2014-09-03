249e29e20ad3d9c70a789cfd01a3240f3150fc0c
Sebastian Alvarez <sebastian.alvarez@hp.com>
2014-09-03 15:41:37 -0600

    new update

--- js/draggable.js
+++ js/draggable.js
@@ -1,35 +1,60 @@
 var dragApp = angular.module('dragModule',[]);
 
-dragApp.directive('draggable',[ '$document',function($document){
-	return function(scope, element, attr){
-		var myScope = scope.messages[attr.id];
-		var startX = 0;
-		var startY = 0;
+dragApp.directive('draggable',['$document',function($document){
+	return {
+		scope: false,
+		link: function(scope, element, attr){
+			console.log(scope);
+			var startX = 0;
+			var startY = 0;
 
-		element.on('mousedown',function(event){
-			event.preventDefault();
-			startX = event.pageX - myScope.myX;
-			startY = event.pageY - myScope.myY;
-			$document.on('mousemove', mousemove);
-			$document.on('mouseup',mouseup);
-			element.addClass("dragged");
-		});
-
-		function mousemove(e){
-			myScope.myX = e.pageX - startX;
-			myScope.myY = e.pageY - startY;
-			element.css({
-				top: myScope.myY + 'px',
-				left: myScope.myX + 'px'
+			element.on('mousedown',function(event){
+				event.preventDefault();
+				startX = event.pageX - scope.myX;
+				startY = event.pageY - scope.myY;
+				$document.on('mousemove', mousemove);
+				$document.on('mouseup',mouseup);
+				element.addClass("dragged");
 			});
-		}
 
-		function mouseup(e){
-			$document.off('mousemove', mousemove);
-			$document.off('mouseup', mouseup);
-			element.removeClass("dragged");
-			scope.messages.$save(myScope);
-			console.log(scope.messages);
+			function mousemove(e){
+				scope.myX = e.pageX - startX;
+				scope.myY = e.pageY - startY;
+				element.css({
+					top: scope.myY + 'px',
+					left: scope.myX + 'px'
+				});
+				scope.$save(scope.myX);
+			}
+
+			function mouseup(e){
+				$document.off('mousemove', mousemove);
+				$document.off('mouseup', mouseup);
+				element.removeClass("dragged");
+				console.log(scope.messages);
+			}
+
+		}
+	};
+}]);
+
+dragApp.directive('moving', ['$scope', function($scope){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+		// template: '',
+		// templateUrl: '',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function(scope, iElm, iAttrs, controller) {
+			
 		}
 	};
 }]);
--- js/message.js
+++ js/message.js
@@ -6,22 +6,51 @@ messageApp.controller('MessageCtrl',function($scope, $firebase){
 	$scope.messages = sync.$asArray();
 });
 
-messageApp.directive('btMessage', function(){
+messageApp.directive('btMessage', function($document,$firebase){
 	return {
-		scope: true,
+		scope: {
+			message:"=",
+			color: "=",
+			author:"=",
+			created:"=",
+			myX: "=",
+			myY: "="
+		},
 		restrict: 'E',
 		replace: true,
 		templateUrl: 'message.html',
-		link: function($scope, iElm, iAttrs, controller) {
-			var id = iAttrs.id;
-			var child = iElm.children(1);
-			console.log($scope.messages[id].color);
-			iElm.css({
-					  backgroundColor: $scope.messages[id].color,
-					  top: $scope.messages[id].myY + 'px',
-					  left: $scope.messages[id].myX + 'px'
-					});
-			/*child[1].css({borderBottom: "20px solid #379BFF"});REVISAR*/ 
+		link: function(scope, element, attr){
+			console.log(scope);
+			var startX = 0;
+			var startY = 0;
+			var ref = new Firebase("https://bubbletalk.firebaseio.com/messages/"+attr.id);
+			var sync = $firebase(ref);
+
+			element.on('mousedown',function(event){
+				event.preventDefault();
+				startX = event.pageX - scope.myX;
+				startY = event.pageY - scope.myY;
+				$document.on('mousemove', mousemove);
+				$document.on('mouseup',mouseup);
+				element.addClass("dragged");
+			});
+
+			function mousemove(e){
+				scope.myX = e.pageX - startX;
+				scope.myY = e.pageY - startY;
+				element.css({
+					top: scope.myY + 'px',
+					left: scope.myX + 'px'
+				});
+				sync.$update({myX: scope.myX, myY: scope.myY});
+			}
+
+			function mouseup(e){
+				$document.off('mousemove', mousemove);
+				$document.off('mouseup', mouseup);
+				element.removeClass("dragged");
+			}
+
 		}
 	};
 });
